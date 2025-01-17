2cfad0410760c16005ba38790fe86f3006be4d21
nmiller15 <nmiller@certifiedangusbeef.com>
2025-01-17 00:20:24 -0500

    add script

---
+++ scripts/push_to_github.ps1
@@ -0,0 +1,25 @@
+# Define source and destination paths
+$sourcePath = "C:\Users\NMiller\OneDrive - CAB\Documents\Vault\blog-posts"
+$destinationPath = "C:\Users\NMiller\OneDrive - CAB\Documents\blog-nolan\content\posts"
+
+# Move the files from source to destination
+Write-Host "Moving files from $sourcePath to $destinationPath"
+robocopy $sourcePath $destinationPath /E /Z /MIR
+
+# Execute Hugo build
+Write-Host "Running Hugo build..."
+hugo
+
+# Change to the GitHub repository directory
+Set-Location -Path "C:\Users\NMiller\OneDrive - CAB\Documents\blog-nolan"
+
+# Add all files to Git
+git add -A
+
+# Commit changes with a message including the current date
+$commitMessage = "Automated commit on $(Get-Date -Format 'yyyy-MM-dd')"
+git commit -m $commitMessage
+
+# Push changes to GitHub
+Write-Host "Pushing changes to GitHub..."
+git push
