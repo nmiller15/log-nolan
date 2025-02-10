cb9ec0f61d3b9d568fc45f30fd74391b8529316b
nmiller15 <nmiller@certifiedangusbeef.com>
2025-02-10 13:14:46 -0500

    update powershell syntax

--- scripts/pushblog.ps1
+++ scripts/pushblog.ps1
@@ -24,37 +24,37 @@ log "Executing blog pipeline script"
 log "Committing source file changes from $MARKDOWN_DIR"
 Set-Location -Path $MARKDOWN_DIR
 
-run_safe "git pull"
+run_safe git pull
 
-run_safe "git add -A"
+run_safe git add -A
 $commit_message = "Automated commit on $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
-run_safe "git commit -m '$commit_message'"
+run_safe git commit -m $commit_message
 
-run_safe "git push"
+run_safe git push
 
 # Move the files from source to destination
 log "Moving files from $MARKDOWN_DIR to $CONTENT"
-run_safe "robocopy $MARKDOWN_DIR $CONTENT /MIR"
+run_safe robocopy $MARKDOWN_DIR $CONTENT /MIR
 
 # Change to the GitHub repository directory
 Set-Location -Path $HUGO_SITE_DIR
 
 log "Pulling changes from GitHub repository"
-run_safe "git pull"
+run_safe git pull
 
 log "Running Hugo build..."
-run_safe "hugo"
+run_safe hugo
 
 log "Committing changes to GitHub repository"
-run_safe "git add -A"
+run_safe git add -A
 $commit_message = "Automated commit on $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
-run_safe "git commit -m '$commit_message'"
-run_safe "git push"
+run_safe git commit -m '$commit_message'
+run_safe git push
 
 # Change to the scripts directory
 log "Pushing changes to DEV.to"
 Set-Location -Path $SCRIPTS
-run_safe "python3 pushtodev.py '$CONTENT'"
+run_safe python3 pushtodev.py '$CONTENT'
 
 # Return to the original directory
 log "Blog pipeline exited successfully"
