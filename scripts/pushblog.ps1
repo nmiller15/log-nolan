4073b38ea049da0bd6b12f1f1ca2291ae0c4767d
nmiller15 <nmiller@certifiedangusbeef.com>
2025-02-10 13:16:21 -0500

    remove run_safe from powershell

--- scripts/pushblog.ps1
+++ scripts/pushblog.ps1
@@ -10,51 +10,42 @@ function log {
     Write-Host "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - $message"
 }
 
-function run_safe {
-    param($command)
-    & $command
-    if ($LASTEXITCODE -ne 0) {
-        log "Error executing: $command"
-        exit 1
-    }
-}
-
 log "Executing blog pipeline script"
 
 log "Committing source file changes from $MARKDOWN_DIR"
 Set-Location -Path $MARKDOWN_DIR
 
-run_safe git pull
+git pull
 
-run_safe git add -A
+git add -A
 $commit_message = "Automated commit on $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
-run_safe git commit -m $commit_message
+git commit -m $commit_message
 
-run_safe git push
+git push
 
 # Move the files from source to destination
 log "Moving files from $MARKDOWN_DIR to $CONTENT"
-run_safe robocopy $MARKDOWN_DIR $CONTENT /MIR
+robocopy $MARKDOWN_DIR $CONTENT /MIR
 
 # Change to the GitHub repository directory
 Set-Location -Path $HUGO_SITE_DIR
 
 log "Pulling changes from GitHub repository"
-run_safe git pull
+git pull
 
 log "Running Hugo build..."
-run_safe hugo
+hugo
 
 log "Committing changes to GitHub repository"
-run_safe git add -A
+git add -A
 $commit_message = "Automated commit on $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
-run_safe git commit -m '$commit_message'
-run_safe git push
+git commit -m '$commit_message'
+git push
 
 # Change to the scripts directory
 log "Pushing changes to DEV.to"
 Set-Location -Path $SCRIPTS
-run_safe python3 pushtodev.py '$CONTENT'
+python3 pushtodev.py '$CONTENT'
 
 # Return to the original directory
 log "Blog pipeline exited successfully"
